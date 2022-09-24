cd3f4a07e4c233182cfc4c13f8c03bda59ace5af
gizmore <gizmore@wechall.net>
2022-09-24 11:52:34 +0200

    '7.0.1-r1595 sync'

--- Method/Form.php
+++ Method/Form.php
@@ -35,7 +35,7 @@ final class Form extends MethodForm
 		$names = [];
 		foreach (GDO_User::admins() as $admin)
 		{
-			$names[] = GDT_ProfileLink::make()->level()->user($admin)->nickname()->avatarUser($admin)->renderHTML();
+			$names[] = GDT_ProfileLink::make()->level()->user($admin)->nickname()->avatarUser($admin)->avatarSize(28)->renderHTML();
 		}
 		$names = implode(',', $names);
 		$email = Module_Contact::instance()->cfgEmail();
