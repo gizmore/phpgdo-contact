40d904618e29186bcab393761c0e22c559aa43d8
gizmore <gizmore@wechall.net>
2026-09-18 04:05:17 +0200

    'more korean'

---
+++ lang/contact_ko.php
@@ -0,0 +1,42 @@
+<?php
+namespace GDO\Contact\lang;
+
+return [
+	'link_contact' => '연락처',
+	'link_message' => '쇼',
+	'contact_info' => '여기에서 관리자에게 연락할 수 있습니다: %s. 또는 %s에 직접 메일을 쓸 수도 있습니다.',
+	'mt_contact_form' => '연락처',
+	'msg_contact_mail_sent' => '귀하의 메시지가 %s의 관리자에게 전송되었습니다.',
+	'card_title_contact_message' => '연락처 메시지',
+	'msg_by' => '메시지 보낸 사람: %s',
+	'msg_title' => '제목: %s',
+	'mail_subj_contact' => '[%s] 연락처 메일',
+	'mail_body_contact' => '친애하는 %s님,
+	
+%s의 문의 양식이 사용되었으며 메시지가 전송되었습니다.
+	
+보낸 사람: %s <%s>
+	
+제목: %s
+<시간/>
+%s
+<시간/>
+친절한 감사
+%2$s 팀',
+	'contact_mail_sender_name' => '%s 지원',
+	'contact_mail_receiver_name' => '%s 지원',
+	'cfg_contact_captcha' => '보안문자가 필요합니까?',
+	'cfg_member_captcha' => '회원을 위한 보안문자?',
+	'cfg_contact_mail' => '직접 연락을 위한 이메일',
+	'cfg_contact_mail_sender' => '이메일 발신자',
+	'cfg_contact_mail_receiver' => '메일링 리스트 수신을 위한 이메일',
+	'tt_cfg_contact_mail_receiver' => '설정된 경우 이는 연락처 메일링 프로세스에서 유일한 수신자 메일입니다.',
+	'md_contact_form' => '%s의 관리자에게 문의하세요',
+	'list_contact_messages' => '%s 연락처 메시지',
+	'cfg_div_whatsapp' => 'WhatsApp 설정',
+	'cfg_whatsapp_settings' => 'WhatsApp 프로필 설정?',
+	'cfg_whatsapp_contact' => 'WhatsApp 연락처 데이터',
+	'whatsapp_number' => '왓츠앱',
+	'my_telegram' => '텔레그램',
+	'telegram' => '텔레그램',
+];
